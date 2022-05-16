9802d8566898409a4d70a29f63944ceeff8c1478
Amber Zimmerman <ajz1221@gmail.com>
2022-05-16 12:08:16 -0600

    creating tasks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Click");
+  };
   return (
-    <header>
-      <h1 style={headingStyle}>{title}</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
@@ -16,9 +21,10 @@ Header.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
-const headingStyle = {
-  color: "red",
-  backgroundColor: "black",
-};
+// css in js
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
 
 export default Header;
